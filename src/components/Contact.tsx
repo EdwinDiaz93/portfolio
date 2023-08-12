07a6b73bc02639d5c3d59318dc8b3d747911e17a
EdwinDiaz93 <josephph1993@gmail.com>
2023-08-12 13:53:14 -0600

    added form validation

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,52 +1,54 @@
-import { motion } from "framer-motion"
 import { useState, useRef } from "react"
+import { useForm } from "react-hook-form"
+import { motion } from "framer-motion"
 import emailjs from '@emailjs/browser'
 import { styles } from "../styles"
 import { EarthCanvas } from "./canvas"
 import { SectionWrapper } from "../hoc"
-import { fadeIn, slideIn } from "../utils/motion"
+import { slideIn } from "../utils/motion"
+
+
+interface formValues {
+  name: string;
+  email: string;
+  message: string
+}
 
 const Contact = () => {
 
-
-  const formRef = useRef();
-  const [form, setForm] = useState({
-    name: '',
-    email: '',
-    message: ''
+  const form = useForm<formValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      message: '',
+    }
   });
-  const [loading, setLoading] = useState(false);
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({
-      ...form,
-      [name]: value
-    })
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
+
+  const { register, handleSubmit, formState, reset } = form;
+  const { errors, isSubmitting } = formState;
+
+
+  const onSubmitForm = (data: formValues) => {
+
     emailjs.send(
       'service_4lx87kg',
       'template_4az0a5m',
       {
-        from_name: form.name,
+        from_name: data.name,
         to_name: 'Edwin',
-        from_email: form.email,
+        from_email: data.email,
         to_email: 'josephph1993@gmail.com',
-        message: form.message,
+        message: data.message,
       },
       'YPDbJGiJZacDI5CFF'
     ).then(() => {
-      setLoading(false);
       alert('Thank you. I will get back to you  as soon as possible');
-      setForm({
+      reset({
         name: '',
         email: '',
-        message: ''
+        message: '',
       })
     }).catch((err) => {
-      setLoading(false);
       console.log(err);
       alert('Something wen wrong');
     })
@@ -61,9 +63,8 @@ const Contact = () => {
         <p className={`${styles.sectionSubText}`}>Get in touch</p>
         <h3 className={`${styles.sectionHeadText}`}>Contact.</h3>
         <form
-        autoCapitalize="off"
-          ref={formRef}
-          onSubmit={handleSubmit}
+          autoComplete="off"
+          onSubmit={handleSubmit(onSubmitForm)}
           className="mt-12 grid grid-cols-1 gap-8"
         >
           <div className="grid grid-cols-1">
@@ -71,41 +72,63 @@ const Contact = () => {
             <input
               id="name"
               type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
+              {...register('name', {
+                required: {
+                  value: true,
+                  message: 'The name field is required'
+                },
+                minLength: {
+                  value: 5,
+                  message: 'You have to provide at least 5 characteres'
+                }
+              })}
               placeholder="Whats your name?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
+            <p className="font-semibold text-red-500 text-start">{errors.name?.message}</p>
           </div>
           <div className="grid grid-cols-1">
             <label htmlFor="email">Your Email:</label>
             <input
               id="email"
               type="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
+              {...register('email', {
+                required: {
+                  value: true,
+                  message: 'the email field is required'
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  message: 'invalid email',
+                },
+              })}
               placeholder="email@example.com"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
+            <p className="font-semibold text-red-500 text-start">{errors.email?.message}</p>
           </div>
           <div className="grid grid-cols-1">
             <label htmlFor="message">Message:</label>
             <textarea
               id="message"
-              name="message"
-              value={form.message}
-              onChange={handleChange}
+              rows={5}
+              {...register('message', {
+                required: {
+                  value: true,
+                  message: 'the message field is required'
+                }
+              })}
               placeholder="type some text..."
               className="resize-none bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
+            <p className="font-semibold text-red-500 text-start">{errors.message?.message}</p>
           </div>
           <button
             type="submit"
+            disabled={isSubmitting}
             className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
           >
-            {loading ? 'Sending' : 'Send'}
+            {isSubmitting ? 'Sending' : 'Send'}
           </button>
         </form>
       </motion.div>
